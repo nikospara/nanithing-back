07089c297c67549f3497a792d13252437e8537cb
Nikos Paraskevopoulos <nikospara@yahoo.com>
2019-09-05 22:36:23 +0300

    The JAX-RS interfaces project

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # nanithing-back
 
 A backend for the Name-Animal-Thing game we used to play in school, implemented with enterprise technologies.
+
+## Launching
+
+Development launch:
+
+```shell
+mvn process-classes quarkus:dev
+```
+
+Note that it requires the `process-classes` goal, not just `compile`. The reason is that at least the `nanithing-jaxrs-interfaces`	
+project needs to be indexed by Jandex, in order to activate the JAX-RS resources that implement those interfaces, and the Jandex goal
+runs in the `process-classes` phase, which is right after `compile` [by default](https://maven.apache.org/guides/introduction/introduction-to-the-lifecycle.html).
--- nanithing-web/src/main/java/nanithing/web/GreetingResourceImpl.java
+++ nanithing-web/src/main/java/nanithing/web/GreetingResourceImpl.java
@@ -1,6 +1,5 @@
 package nanithing.web;
 
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static nanithing.model.GameState.BUILDING;
 import static nanithing.model.GameVisibility.PUBLIC;
 
@@ -8,18 +7,14 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-
 import nanithing.model.Game;
 import nanithing.model.ImmutableGame;
 
-@Path("/hello")
-public class GreetingResource {
+/**
+ * Implementation of the {@link GreetingResource}.
+ */
+public class GreetingResourceImpl implements GreetingResource {
 
-	@GET
-	@Produces(APPLICATION_JSON)
 	public CompletionStage<Game> hello() {
 		return CompletableFuture.completedStage(ImmutableGame.builder()
 				.id(UUID.randomUUID().toString())
